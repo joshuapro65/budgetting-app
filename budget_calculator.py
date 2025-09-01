441eec4931a12c97c43f4d2c46610608420d5cc4
Rai <joshuawalters595@gmail.com>
2025-08-31 19:47:41 -0500

    Improve math and expense data storage

--- budget_calculator.py
+++ budget_calculator.py
@@ -10,18 +10,18 @@ expenses = float(input("Please Enter the Cost of your Expenses:\n$"))
 other_expenses_for = input("Are there any other expenses that you wish to enter?\nPlease enter Y for Yes or N for No.")
 
                         
-if other_expenses_for == "Yes":
-    print(input("Please enter what the Expense is for:\n"))
-    other_expenses = float(input(f"Please enter the cost of the Expense: $"))
-    expenses = income - (other_expenses + expenses)
+if other_expenses_for == "Y":
+    other_expenses = input("Please enter what the Expense is for:\n")
+    other_expenses_cost = float(input(f"Please enter the cost of the Expense: $"))
+    expenses = other_expenses_cost + expenses
 
-if other_expenses_for == "No":
-    expenses == expenses
+if other_expenses_for == "N":
+    expenses = expenses
 
 
 # Budget Calculation
 remaining = income - expenses
 
 # View Results
-print(f"Your total expenses will be ${expenses}, these will be for {expense_for}")
+print(f"Your total expenses will be ${expenses}, these will be for {expense_for}.\n")
 print(f"Your remaining budget is: ${remaining}")
