8f7fda46d9eaadce7616bc80874e7a77dfe040ba
Jordan <joshuawalters595@gmail.com>
2025-09-14 19:10:45 -0500

    Updated the logical thinking of the app. As well as making it possible to add multiple Expense Entries without any issues.

--- budget_calculator.py
+++ budget_calculator.py
@@ -5,23 +5,48 @@ print("Welcome to your Budgeting Calculator!!!")
 income = float((input("Please Enter Your Income below:\n$")))
 
 # Initial Expenses 
-expense_for = input("Please Enter What The Expense is For:\n")
+expense_for = [input("Please Enter What The Expense is For:\n")]
 expenses = float(input("Please Enter the Cost of your Expenses:\n$"))
-other_expenses_for = input("Are there any other expenses that you wish to enter?\nPlease enter Y for Yes or N for No.")
 
-                        
-if other_expenses_for == "Y":
-    other_expenses = input("Please enter what the Expense is for:\n")
-    other_expenses_cost = float(input(f"Please enter the cost of the Expense: $"))
-    expenses = other_expenses_cost + expenses
+# Checks if you have enough money for your expenses
+if income < expenses:
+    print("You don't have enough money for this!!!")
+    exit()
 
-if other_expenses_for == "N":
-    expenses = expenses
 
+# Addition of another or multiple expenses
+other_expenses_for = input("Are there any other expenses that you wish to enter?\nPlease enter Y for Yes or N for No.\n").upper()
+
+
+# While loop to continue asking if the user wants to add another expense
+while True:
+    if other_expenses_for == "Y":
+        expense_for.append(input("Please enter what the Expense is for:\n"))
+        other_expenses_cost = float(input(f"Please enter the cost of the Expense:\n$"))
+        expenses += other_expenses_cost
+
+        other_expenses_for = input("Are there any other expenses that you wish to enter?\nPlease enter Y for Yes or N for No.\n").upper() # makes it so that the user's input doesn't give an error for a y entry
+
+    elif other_expenses_for == "N":
+        print("Processing your data......")
+        break
+    else:
+        other_expenses_for = input("You entered and invalid input. Please enter Y for Yes or N for No!!!\n").upper() # checks the user's input to see if there was invalid entries 
 
 # Budget Calculation
 remaining = income - expenses
 
+# Checks if your expenses are more than what you have allocated
+if remaining <= 0:
+    print("You will exceed your total income...")
+    cont = input("Do you want to continue? Please enter Y for Yes or N for No.\n").upper()
+    if cont == "Y":
+        print("Ok")
+    elif cont == "N":
+        print("Ok")
+        exit()
+
 # View Results
-print(f"Your total expenses will be ${expenses}, these will be for {expense_for}.\n")
+print("\n")
+print(f"Your total expenses will be ${expenses}, these will be for {expense_for}: {expenses}.\n")
 print(f"Your remaining budget is: ${remaining}")
